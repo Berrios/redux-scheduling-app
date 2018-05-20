602f4450f78d69709cabccd0acfb02a74c11ce53
Ben Pardo <visaudi@gmail.com>
2018-05-19 18:53:52 -0600

    Tests a vote increase.

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 const { createStore } = require("redux");
 const voteCounter = require("./vote-counter");
 const store = createStore(voteCounter);
+const {increaseVote} = require("./increase-vote");
 
 
+test("starts with no votes", function() {
+    expect(store.getState().count).toBe(0);
+});
 
 test("increases vote by one", function() {
-    expect(store.getState().count).toBe(0);
+    store.dispatch(increaseVote());
+    expect(store.getState().count).toBe(1);
 });
